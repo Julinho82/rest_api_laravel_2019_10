e08daf622fea0110e0de147605daef721b38a77e
Julio Daviú Arévalo <juliolotr@gmail.com>
2019-10-24 01:07:42 -0400

    magament field table modo_pago

--- app/ModoPago.php
+++ app/ModoPago.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ModoPago extends Model
 {
     //
+    protected $table='modo_pagos';
+
+    protected $filleable=['nombre','otros_detalles'];
+
+    protected $hidden=['id','created_at','updated_at'];
 }
